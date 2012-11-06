f35bdd8635b4dd45b86086dd91115c8f5ea54c9e
An Dang <adang@constantcontact.com>
2012-11-06 18:05:32 -0500

    adding gravatar for User model

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "An Dang"
+    email    "dang.an249@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,17 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  describe "show profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before do 
+      visit user_path(user) 
+      puts user.name
+    end
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
 end
